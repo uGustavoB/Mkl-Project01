1dc2b9b4aca1d2461ea0a614318d97181b2f0acf
franciscovmn <fvmaianeto@gmail.com>
2023-11-06 12:08:50 -0300

    teste

--- menu/pages/js-menu-mobile/menu-mobile.js
+++ menu/pages/js-menu-mobile/menu-mobile.js
@@ -14,7 +14,7 @@ class MobileNavbar {
           ? (link.style.animation = "")
           : (link.style.animation = `navLinkFade 0.5s ease forwards ${
               index / 7 + 0.3
-            }s`);
+            }s`); 
       });
     }
   
